5faf055537332de65d1e7ff7cc713927871294be
Sean McDermott <seanjmac@hotmail.com>
2018-04-19 16:37:37 -0700

    add project skeleton to github

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { project } from './project';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+});
