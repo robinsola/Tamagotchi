4c4146e36de0fdf03fb89ade0e48c2cab026a4c2
Joe McKinney and Robin Sola <robin@>
2018-04-23 11:14:09 -0700

    create project environment and comment out index.html and  main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { project } from './project';
-import './styles.css';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-$(document).ready(function() {
-
-});
+// import { project } from './project';
+// import './styles.css';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//
+// $(document).ready(function() {
+//
+// });
