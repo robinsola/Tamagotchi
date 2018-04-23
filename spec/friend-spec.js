3820920501b79046775a3a3dca5d1e58555547ec
Joe McKinney and Robin Sola <robin@>
2018-04-23 13:33:01 -0700

    add a play and a feed function to logic.js that will feed the friend and add energy or play with friend and will make their mood better but will make them more tired.

--- spec/friend-spec.js
+++ spec/friend-spec.js
@@ -19,6 +19,8 @@ describe('friend', function() {
     expect(newFriend.energyLevel).toEqual(20);
   });
 
+
+
   it('should reduce foodLevel by one every second', function() {
     newFriend.getHungry();
     jasmine.clock().tick(2001);
@@ -37,5 +39,28 @@ describe('friend', function() {
     expect(newFriend.energyLevel).toEqual(18);
   });
 
+
+});
+describe('friend', function() {
+  let newFriend = new Friend('Tommi');
+
+  beforeEach(function() {
+     newFriend.hungerLevel = 10;
+     newFriend.moodLevel = 10;
+     newFriend.energyLevel = 20;
+
+  });
+
+  it('should increase hungerLevel by 3 and increase energyLevel by 3', function() {
+    newFriend.feed();
+    expect(newFriend.hungerLevel).toEqual(13);
+    expect(newFriend.energyLevel)toEqual(23);
+  });
+
+  it('should increase moodLevel by 3 and decrease hunger level by 3', function() {
+    newFriend.play();
+    expect(newFriend.moodLevel).toEqual(13);
+    expect(newFriend.energyLevel)toEqual(17);
+  });
 
 });
--- src/logic.js
+++ src/logic.js
@@ -9,13 +9,29 @@ class Friend {
   getHungry() {
     setInterval(() => {
       this.hungerLevel--;
-      }, 1000);
-    }
+    }, 1000);
+  }
 
   getBored() {
     setInterval(() => {
       this.moodLevel--;
-      }, 1000);
-    }
+    }, 1000);
   }
+
+  getTired() {
+    setInterval(() => {
+      this.energyLevel--;
+    }, 1000);
+  }
+
+  feed() {
+    this.hungerLevel += 3;
+    this.energyLevel += 3;
+  }
+
+  play() {
+    this.moodLevel += 3;
+    this.energyLevel -= 3;
+  }
+}
 export { Friend };
