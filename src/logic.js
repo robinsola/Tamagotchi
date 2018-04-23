e079e3c09639fd6fb91f0751a299eeaee8e03da0
Joe McKinney and Robin Sola <joe+robin@>
2018-04-23 11:16:47 -0700

    add constructor to logic.js for first spec, pass.

--- src/logic.js
+++ src/logic.js
@@ -1,3 +1,9 @@
-function joesFunction() {
-  
+class Friend {
+  constructor(name) {
+    this.name = name;
+    this.hungerLevel = 10;
+    this.moodLevel = 10;
+    this.energyLevel = 20;
+  }
 }
+export { Friend }
