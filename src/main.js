1e92d77f0aa3d81a3c2976d8ffc17daf17502f20
Joe McKinney, Robin Sola and semhar kidane <robin@>
2018-04-24 10:30:59 -0700

    add constructor variable.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-// import { project } from './project';
-// import './styles.css';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-//
-// $(document).ready(function() {
-//
-// });
+import { Friend } from './logic';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+  $("#user-input").submit(function(event){
+    event.preventDefault();
+
+    let name = $("#name-input").val();
+    let newFriend = new Friend(name);
+
+    console.log(newFriend);
+
+  });
+});
